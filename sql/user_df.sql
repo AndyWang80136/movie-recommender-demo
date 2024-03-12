1309ace1871f82a6d7a1da3587a05172ca8b453d
AndyWang(Wang Chun-Hao) <andy80136@gmail.com>
2024-03-12 16:20:51 +0800

    update: sql files

--- sql/user_df.sql
+++ sql/user_df.sql
@@ -2,13 +2,7 @@ SELECT
     users.user_id,
     users.gender,
     ages.age_interval,
-    occupations.occupation,
-    train_count,
-    val_count,
-    test_count
-FROM user_phase_count
-JOIN users USING (user_id)
+    occupations.occupation
+FROM users
 JOIN occupations USING (occupation_id)
 JOIN ages USING (age_id)
-WHERE test_count != 0
-ORDER BY train_count DESC
